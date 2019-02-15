cfb7b521de2104390b20e143f9e19147bf152b17
Ronnel Marfil <ronnel@sym.ph>
2019-02-15 15:50:59 +0800

    Adjusted users query

--- app/graphql/schemas/user.py
+++ app/graphql/schemas/user.py
@@ -23,38 +23,31 @@ class Query(graphene.ObjectType):
     user = graphene.Field(
         UserType,
         id=graphene.ID(description="The NDB ID of a User"), required=True)
-    users = graphene.List(
+    users = NdbConnectionField(
         UserType,
         description="Gets all users")
 
     def resolve_user(self, info, id, **kwargs):
         user = User.get_by_id(int(id))
         if not user:
-            raise Exception("Gagu")
+            raise Exception("User not found")
         return user
 
     def resolve_users(self, info, **kwargs):
-        return User.query().fetch()
+        return User.query()
 
 
 class AddUser(graphene.relay.ClientIDMutation):
     class Input:
-        first_name = graphene.String()
-        last_name = graphene.String()
-        email = graphene.String()
+        username = graphene.String()
         password = graphene.String()
+        user_type = graphene.String()
 
     user = graphene.Field(UserType)
 
     @classmethod
-    def mutate_and_get_payload(cls, root, info, first_name, last_name, email, password, **kwargs):
-        user = User()
-        user.first_name = first_name
-        user.last_name = last_name
-        user.email = email
-        user.password = password
-        user.put()
-
+    def mutate_and_get_payload(cls, root, info, username, password, user_type, **kwargs):
+        user = User.add(username=username, password=password, user_type=user_type)
         return AddUser(user=user)
 
 
